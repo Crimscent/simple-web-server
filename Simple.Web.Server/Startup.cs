e08f666ed16ee5f21a936100cd0a3e43aa44644a
Crimscent <esudakov@gmail.com>
2016-10-08 22:55:12 +0300

    Fixed IIS publishing, removed server=kestrel header.

--- Simple.Web.Server/Startup.cs
+++ Simple.Web.Server/Startup.cs
@@ -30,6 +30,7 @@ namespace Simple.Web.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseDowntime(Configuration);
             app.UseStaticFiles();
             app.UseIndex();
             app.UseStaticFiles();
