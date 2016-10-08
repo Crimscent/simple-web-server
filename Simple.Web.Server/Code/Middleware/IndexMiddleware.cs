f381e83d1422754abb97e9f9f9fa82f2d2592b4a
Crimscent <esudakov@gmail.com>
2016-10-08 22:19:45 +0300

    Initial commit.

---
+++ Simple.Web.Server/Code/Middleware/IndexMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Simple.Web.Server.Middleware
+{
+    public class IndexMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly Func<HttpContext, bool> _predicate;
+
+        public IndexMiddleware(RequestDelegate next, Func<HttpContext, bool> predicate)
+        {
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            _next = next;
+            _predicate = predicate;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            if (_predicate(context))
+            {
+                context.Request.Path = "/index.html";
+            }
+
+            return _next(context);
+        }
+    }
+}
---
+++ Simple.Web.Server/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Simple.Web.Server.Extensions;
+
+namespace Simple.Web.Server
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+            app.UseIndex();
+            app.UseStaticFiles();
+
+            loggerFactory.AddNLog();
+        }
+    }
+}
