55196a0ad9a655d3292fef8c4f62ea20dbae09d7
Crimscent <esudakov@gmail.com>
2016-10-08 23:19:56 +0300

    Added ping endpoint.

--- Simple.Web.Server/Startup.cs
+++ Simple.Web.Server/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,9 +20,13 @@ namespace Simple.Web.Server
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
             Configuration = builder.Build();
+
+            Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
         }
 
         public IConfigurationRoot Configuration { get; }
+
+        public string Version { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,6 +36,18 @@ namespace Simple.Web.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.Map("/ping", subApp =>
+            {
+                subApp.UseDowntime(Configuration);
+                subApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentType = "text/json";
+
+                    await context.Response.WriteAsync($@"{{""version"":""{Version}""}}");
+                });
+            });
+
             app.UseDowntime(Configuration);
             app.UseStaticFiles();
             app.UseIndex();
